9d6ee3c580e9099b238fdf47ff7f1de0db2b071a
Mohammed Gulam Muhith <78392937+muhithg@users.noreply.github.com>
2024-06-06 00:26:58 +0000

    My Version OF This Course

--- Chapter 1/01_03b.sql
+++ Chapter 1/01_03b.sql
@@ -1,1 +1,11 @@
 -- Insert new grape flavor product into table
+INSERT INTO Product(
+  ProductID, ProductCode,
+  ProductName, Size,
+  Variety, Price, Status
+)
+Value(
+  17, 'MWPRA20',
+  'Mineral Water',20,
+  'Grape', '1.79','ACTIVE'
+)
--- Chapter 1/01_05b.sql
+++ Chapter 1/01_05b.sql
@@ -1,1 +1,7 @@
 -- Find null values in Customer table
+select *
+from Customer
+WHERE FirstName IS NULL
+  or LastName is NULL
+  or email is null
+  or Phone is null;
